b4cace5a12bc60ff55007feb0352759cc58a16f0
KndWajs <knd.wajs@gmail.com>
2021-01-01 23:08:56 +0100

    refactoring_WIP

--- java/pps-cloud-backup/src/main/java/main.java
+++ java/pps-cloud-backup/src/main/java/main.java
@@ -5,111 +5,72 @@ import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class main {
 
     public static final int NEWEST_FILE_INDEX = 0;
+    public static final int NUMBER_OF_FILES_TO_SYNC = 15;
+    public static final int NUMBER_OF_OLDEST_FILES = 2;
+    public static final int OLDEST_FILE_IN_DAYS = 90;
+
 
     public static void main(String args[]) throws IOException {
         final String FILES_PATH = args[0];
         System.out.println("Synchronize files with AWS");
 
         //check if its first logging if yes than ask about credentials to AWS
+
         //get files
-        //remain last 15 files + two oldest but not oldest than 3Mo
+        //remain 15 files - two oldest but not oldest than 3Mo and rest the newest
+        List<File> localRelevantFiles = getRelevantLocalFiles(FILES_PATH);
+        showMessageAboutFilesToSync(localRelevantFiles);
+
         //synchronize files with AWS
 
     }
 
-    public static void  trash(String args[]) {
-        final String FILES_PATH = args[0];
-        File[] files = new File(FILES_PATH).listFiles();
-        Arrays.sort(files, Comparator.comparingLong(File::lastModified).reversed());
+    private static List<File> getRelevantLocalFiles(String path) {
+        List<File> files = new ArrayList<>(new File(path).listFiles());
+
+        if (files.size() <= NUMBER_OF_FILES_TO_SYNC) {
+            return files;
+        }
+
+        List<File> oldestFiles = findOldestRelevantFiles(files);
+
+
+        return files;
+    }
+
+    private static List<File> findOldestRelevantFiles(List<File> files) {
+        List<File> receivedFiles = new ArrayList<>(files);
+        Collections.sort(receivedFiles, Comparator.comparingLong(File::lastModified).reversed());
+        receivedFiles.subList(receivedFiles.size() - NUMBER_OF_FILES_TO_SYNC, receivedFiles.size()).clear();
+
+        ArrayList<File> oldestFiles = receivedFiles.stream().filter(file -> file.lastModified() >
+                new Date().getTime() - TimeUnit.DAYS.toMillis(OLDEST_FILE_IN_DAYS))
+                .collect(Collectors.toCollection(ArrayList::new));
+
+
+        if (oldestFiles.size() <= NUMBER_OF_FILES_TO_SYNC) {
+            return new ArrayList<>();
+        } else if (files.size() <= NUMBER_OF_FILES_TO_SYNC + 1) {
+            return files.get(0).lastModified() < new Date().getTime() ?
+        }
+
+        return new File[0];
+    }
+
+    private static void showMessageAboutFilesToSync(List<File> files) {
+        System.out.println("Files to synchronization:");
         int i = 0;
         for (File f : files) {
             i++;
-            System.out.println(i + " - " + f.getName());
-            System.out.println(i + " - " + f.lastModified());
-        }
-
-
-//        AWS
-        System.setProperty("aws.accessKeyId", "*");
-        System.setProperty("aws.secretAccessKey", "*");
-        S3Client s3client = S3Client.builder().region(Region.EU_CENTRAL_1).build();
-
-        String bucketName = "kspps-bucket";
-
-        HeadBucketRequest headBucketRequest = HeadBucketRequest.builder()
-                .bucket(bucketName)
-                .build();
-        try {
-            s3client.headBucket(headBucketRequest);
-            System.out.println("jest taki bucket");
-        } catch (NoSuchBucketException e) {
-            s3client.createBucket(CreateBucketRequest.builder()
-                    .bucket(bucketName)
-                    .build());
-            System.out.println("stworzony nowy bucket");
-        }
-
-        String key = files[NEWEST_FILE_INDEX].getName();
-        File to = new File(args[0] + key);
-
-        // Put Object
-        s3client.putObject(PutObjectRequest.builder().bucket(bucketName).key(key)
-                .build(), RequestBody.fromFile(to));
-
-        ListObjectsV2Response objectListing = s3client.listObjectsV2(ListObjectsV2Request.builder().bucket(bucketName).build());
-
-        for(S3Object object : objectListing.contents()) {
-            System.out.println(object.key());
-            System.out.println(object.lastModified());
+            System.out.println(i + " - " + f.getName() + " - " + f.lastModified());
         }
     }
-
-
-    public static void copyFile(String args[]) throws IOException {
-
-        System.out.println(System.getProperty("user.dir"));
-        System.out.println(new File(".").getAbsolutePath());
-
-        File[] files = new File(args[0]).listFiles();
-        Arrays.sort(files, Comparator.comparingLong(File::lastModified));
-        int i = 0;
-        for (File f : files) {
-            i++;
-            System.out.println(i + " - " + f.getName());
-            System.out.println(i + " - " + f.lastModified());
-        }
-        Arrays.sort(files, Comparator.comparingLong(File::lastModified).reversed());
-        for (File f : files) {
-            System.out.println(i + " - " + f.getName());
-            System.out.println(i + " - " + f.lastModified());
-            i--;
-        }
-
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-
-
-        System.out.println("Copying file into another location using Java 7 Files.copy");
-//        copyFile();
-
-
-//        String path = "C:/destination/";
-
-
-//        Path src = Paths.get("target/test_text.txt");
-//        File file = Files.list(Paths.get("target"));
-
-
-        File to = new File("java7.txt");
-//        Files.copy(src, to.toPath(), StandardCopyOption.REPLACE_EXISTING);
-    }
-
 }
 
